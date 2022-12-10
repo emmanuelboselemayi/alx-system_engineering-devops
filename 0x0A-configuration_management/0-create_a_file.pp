7db0174d967349f5a3c5a6454008cb952c7b9493
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-12-10 13:51:00 -0800

    0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,8 @@
-file { '/tmp/school':
-  ensure  => present,
+# task 0. Create a file
+
+file { 'school':
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
