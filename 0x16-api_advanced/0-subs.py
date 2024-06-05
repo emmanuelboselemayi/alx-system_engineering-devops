7782c154abc43187786e0849c2009dae70a00f9f
emmanuelboselemayi <emmanuelboselemayi@gmail.com>
2024-06-04 19:53:02 -0700

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" 
+Queries the Reddit API and returns the  number of subcribers 
+"""
+
+import requests
+    
+    def number_of_subcribers(subreddit):
+        """Queries the Reddit API and returns the number of subcribers
+        to the subbreddit"""
+
+        sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                                .format(subreddit),
+                                headers={"User-Agent": "My-User-Agent"},
+                                allow_redirects=False)
+
+        if sub_info.status_code >= 300:
+            return 0
+
+        return sub_info.json().get("data").get("subscribers")
+
+
