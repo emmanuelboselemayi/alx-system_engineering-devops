db67e67b98b55075274a7648827991d919d805a8
emmanuelboselemayi <emmanuelboselemayi@gmail.com>
2024-06-04 19:55:15 -0700

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ import requests
                                 headers={"User-Agent": "My-User-Agent"},
                                 allow_redirects=False)
 
-        if sub_info.status_code >= 300:
+    if sub_info.status_code >= 300:
             return 0
 
         return sub_info.json().get("data").get("subscribers")
